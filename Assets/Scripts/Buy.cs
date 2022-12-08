11994a4e9cc1d532623fa2468f887e8b9973cb6b
QMJohnson <quintenj0123@gmail.com>
2022-12-07 21:40:02 -0600

    Added purchase sound effect, removed unused sound effect

--- Assets/Scripts/Buy.cs
+++ Assets/Scripts/Buy.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 
 public class Buy : MonoBehaviour
 {
+    [SerializeField] AudioClip _spend;
     public ShopController shopController;
 
     public void buyItem(int price)
     {
         shopController.gold -= price;
+        AudioHelper.PlayClip2D(_spend, 1);
         Debug.Log("SOLD!");
     }
 }
