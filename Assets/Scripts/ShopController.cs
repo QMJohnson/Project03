ce9b219fc1e9c5ae65c8190e7e684edc1489f4bd
QMJohnson <quintenj0123@gmail.com>
2022-12-07 18:57:43 -0600

    Added gold text (should update when I add functions)

--- Assets/Scripts/ShopController.cs
+++ Assets/Scripts/ShopController.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Text _goldTextView;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        //gets highscore and displays it on canvas
+        int gold = 1000;
+        _goldTextView.text = gold.ToString();
     }
 }
